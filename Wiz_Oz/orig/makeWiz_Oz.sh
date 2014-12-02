093c99d165316650e5ce02fc0681257061a8d366
tbuyus <tbuyus@mail.com>
2014-12-02 14:19:16 +0000

    automated a lot of link and filename cleaning and reset toc and content after wipiing

--- Wiz_Oz/orig/makeWiz_Oz.sh
+++ Wiz_Oz/orig/makeWiz_Oz.sh
@@ -1,16 +1,58 @@
 #!/usr/bin/sh
-mv  Wiz_Oz.epub Wiz_Oz.epub.old
-echo "zipping into an epub"
-7za a -tzip Wiz_Oz.epub 43936/*.html 43936/*.css 43936/*.opf 43936/*.ncx META-INF/*.*
-echo "adding non images files to epub"
-echo "Would you like to zip images? [Y/N]" 
+
+echo "sanitizing file names and internal links: removing all gutenberg directory structure from filenames"
+. sanitizeLinks.sh
+
+echo "cleaning original html files"
+
+#getting rid of most divs
+perl -p  -i -e 's/class=\"(shape_)*wrap_(area_)*(left|right)/class=\"/g' 43936/h-*.html
+perl -p  -i -e 's/class=\"fig(center|right)/class=\"/g' 43936/h-*.html
+perl -p  -i -e 's/class=\"(right|left)2* c[0-9][0-9]*[0-9]*/class=\"/g' 43936/h-*.html
+#perl -p  -i -e 's/<div class=\"\">\s*<\/div>//g' 43936/h-*.html
+perl -p  -i -e 's/<\/div>//g' 43936/h-*.html
+perl -p -i -e 's/<div class=\"\">//g' 43936/h-*.html
+
+#getting rid of page numbers
+#perl -p  -i -e 's/<p><(a|span) id=\"Page_[0-9][0-9]*[0-9]*\" class=\"x-epubmaker-pageno\" title=\"\[Pg [0-9][0-9]*\]\"><\/a><\/p>//g' 43936/h-*.html
+#perl -p  -i -e 's/<(a|span) id=\"Page_[0-9][0-9]*[0-9]*\" class=\"x-epubmaker-pageno\" title=\"\[Pg [0-9][0-9]*\]\"><\/a>//g' 43936/h-*.html
+#perl -p  -i -e 's/(<span class=\"pagenum\">)<a id=\"Page_[0-9][0-9]*[0-9]*\">\[Pg [0-9][0-9]*[0-9]*\]<\/a><br\/>//g' 43936/h-*.html
+perl -p  -i -e 's/<p><(a|span) id=\"Page_[0-9][0-9]*[0-9]*\" class=\"x-epubmaker-pageno\" title=\"\[Pg [0-9][0-9]*\]\"><\/ (a|span)><\/p>//g' 43936/h-*.html
+perl -p  -i -e 's/<(a|span) id=\"Page_[0-9][0-9]*[0-9]*\" class=\"x-epubmaker-pageno\" title=\"\[Pg [0-9][0-9]*\]\"><\/(a|span)>//g' 43936/h-*.html
+perl -p  -i -e 's/(<span class=\"pagenum\">)<a id=\"Page_[0-9][0-9]*[0-9]*\">\[Pg [0-9][0-9]*[0-9]*\]<\/a><br\/>//g' 43936/h-*.html
+perl -p  -i -e 's/<a id=\"Page_[0-9][0-9]*[0-9]*\">\[Pg [0-9][0-9]*[0-9]*\]<\/a>(<\/span>)//g' 43936/h-*.html
+
+
+#getting rid of redundant divs around images
+#:wperl -p  -i -e 's/<div class=\"\">(.*)<\/div>/\1/g' 43936/h-*.html
+
+
+
+echo "Would you like to create ebook? [Y/N]" 
 read answer 
 if [ "$answer" == "y" ] || [ "$answer" == "Y" ]; 
-  then 7za a Wiz_Oz.epub -tzip 43936/*.jpg 
-fi
-java -jar ../../../epubcheck-3.0.1/epubcheck-3.0.1.jar  Wiz_Oz.epub
-echo "creating azw3 file for kindle"
-ebook-convert Wiz_Oz.epub Wiz_Oz.azw3
-echo "creating mobi file"
-ebook-convert Wiz_Oz.epub Wiz_Oz.mobi
+  then  mv  Wiz_Oz.epub Wiz_Oz.epub.old
+   echo "zipping into an epub"
+   7za a -tzip Wiz_Oz.epub 43936/*.html 43936/*.css 43936/*.opf 43936/*.ncx META-INF/*.*
+   echo "adding non images files to epub"
+    
+   echo "Would you like to zip images? [Y/N]" 
+   read answer 
+   if [ "$answer" == "y" ] || [ "$answer" == "Y" ]; 
+     then 7za a Wiz_Oz.epub -tzip 43936/*.jpg 
+   fi
 
+   echo "Would you like to check epub validity? [Y/N]" 
+   read answer 
+   if [ "$answer" == "y" ] || [ "$answer" == "Y" ]; 
+     then java -jar ../../../epubcheck-3.0.1/epubcheck-3.0.1.jar  Wiz_Oz.epub 
+   fi
+
+  echo "Would you like to azw3 and mobi files? [Y/N]" 
+  read answer 
+  if [ "$answer" == "y" ] || [ "$answer" == "Y" ]; 
+    then 
+    ebook-convert Wiz_Oz.epub Wiz_Oz.azw3
+    ebook-convert Wiz_Oz.epub Wiz_Oz.mobi
+  fi
+fi
---
+++ Wiz_Oz/orig/sanitizeLinks.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/sh
+echo "sanitizing original html links"
+chmod 666 images/*.jpg
+chmod 666 43936/*.html
+
+rm -r images/*.jpg
+rm -r 43936/*.html
+
+cp orig-43936/*.jpg images/
+cp orig-43936/*.htm.html 43936/
+
+chmod 666 images/*.jpg
+chmod 666 43936/*.htm.html
+#getting rid of most divs
+#perl -p  -i -e 's/class=\"(shape_)*wrap_(area_)*(left|right)/class=\"/g' 43936/h-*.htm
+rename  's/\@public\@vhost\@g\@gutenberg\@html\@files\@43936\@43936\-h\@images\@i//g' images/*.jpg
+rename  's/_edit//g' images/*.jpg
+rename  's/\@public\@vhost\@g\@gutenberg\@html\@files\@43936\@43936\-h\@43936\-//g' 43936/*.html
+perl -p -i -e  's/\@public\@vhost\@g\@gutenberg\@html\@files\@43936\@43936\-h\@43936\-//g' 43936/*.html
+perl -p -i -e  's/\.htm\.html/\.html/g' 43936/*.html
+perl -p -i -e  's/_edit//g' 43936/*.html
+perl -p -i -e  's/\@public\@vhost\@g\@gutenberg\@html\@files\@43936\@43936\-h\@images\@i//g' 43936/*.html
+rename  's/\.htm\.html/\.html/g' 43936/*.html
+
