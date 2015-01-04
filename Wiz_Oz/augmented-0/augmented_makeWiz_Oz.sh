50ef9b1c940b8e573ec0fe1daf3a0e9cb36b9a22
tbuyus <tbuyus@tbuyus-ME05.(none)>
2015-01-04 15:59:09 +0000

    rebuilding freq-filt-aug-0.pl

--- Wiz_Oz/augmented-0/augmented_makeWiz_Oz.sh
+++ Wiz_Oz/augmented-0/augmented_makeWiz_Oz.sh
@@ -1,4 +1,12 @@
-cp -r ../orig/* .
+#this brings all the original files across
+#cp -r ../orig/* .
+
+#this dealt with header unclosed tags - only going to copy body across
+#and reuse headers from first file so dont need meta and link tag
+#fixed
+#perl -p -i -e 's/<(br|hr|meta|link|img)([^>]*)>/<\1\2\/>/g' augmented_HTML/*.html
+
+perl -p -i -e 's/<(br|hr|img)([^>^\/]*)>/<\1\2\/>/g' augmented_HTML/*.html
 cp -r augmented_HTML/*.* 43936/
 
 . makeWiz_Oz.sh
