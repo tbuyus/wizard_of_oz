20255599e60065ca550f0e172f42652c00fdeef8
tbuyus <tbuyus@tbuyus-ME05.(none)>
2015-01-05 16:16:08 +0000

    formatting of aug-0 nearly there

--- Wiz_Oz/freq-filt-aug-0.pl
+++ Wiz_Oz/freq-filt-aug-0.pl
@@ -1,15 +1,24 @@
 !#/usr/bin/perl
 
+#use utf8;
+#use Encode;
+
 #s/\/\/([^\/]*)\/\/([0-9][0-9]*)\/\/([^\/]*)\/\//foo\-$1\-$2-bar-$3-beque/g;
-#s/\/\/([^\/]*)\/\/([0-9][0-9]*)\/\/([^\/]*)\/\//if($2>3000){$1}else{$3}/ge;
+#s/\/\/([^\/]*)\/\/([0-9][0-9]*)\/\/([^\/]*)\/\//if($2>30){$1}else{$3}/ge;
 #s/\/\/([^\/]*)\/\/([0-9][0-9]*)\/\/([^\/]*)\/\//if($2>30){&graySilentLetters($1)}else{$3}/ge;
-s/\/\/([^\/]*)\/\/([0-9][0-9]*)\/\/([^\/]*)\/\//if($2>30){&processRhotics(&graySilentLetters(addSuperscripts($1)))}else{$3}/ge;
+#s/\/\/([^\/]*)\/\/([0-9][0-9]*)\/\/([^\/]*)\/\//if($2>30){&processRhotics(&graySilentLetters(&addSuperscripts($1)))}else{$3}/ge;
+#s/\/\/([^\/]*)\/\/([0-9][0-9]*)\/\/([^\/]*)\/\//if($2>30){&graySilentLetters(&addSuperscripts($1))}else{$3}/ge;
+#s/\/\/([^\/]*)\/\/([0-9][0-9]*)\/\/([^\/]*)\/\//if($2>30){&addSuperscripts($1)}else{$3}/ge;
+#s/\/\/([^\/]*)\/\/([0-9][0-9]*)\/\/([^\/]*)\/\//if($2>30){&processRhotics(&graySilentLetters(&addSuperscripts($1)))}else{$3}/ge;
+#s/\/\/([^\/]*)\/\/([0-9][0-9]*)\/\/([^\/]*)\/\//if($2>30){&addSuperscripts(&processRhotics(&graySilentLetters($1)))}else{$3}/ge;
+s/\/\/([^\/]*)\/\/([0-9][0-9]*)\/\/([^\/]*)\/\//if($2>30){&processRhotics(&addSuperscripts(&graySilentLetters($1)))}else{$3}/ge;
 
 
 
 sub graySilentLetters {
 $word=shift; 
-$word=~ s/([^\/0-9])0/<tt>$1<\/tt>/g;
+$word=~ s/([^0-9][^0-9]*)0/<tt>$1<\/tt>/g;
+#$word=~ s/([a-zA-Z])0/<tt>$1<\/tt>/g;
 $word=~ s/(à)/<tt>$1<\/tt>/gi;
 $word=~ s/(è)/<tt>$1<\/tt>/gi;
 $word=~ s/(ì)/<tt>$1<\/tt>/gi;
@@ -23,7 +32,8 @@ return $word;
 
 sub addSuperscripts   {
 $word=shift; 
-$word=~ s/([^\/]*)\!([^\/]*)/$1<sup>$2<\/sup>/g;
+#$word=~ s/([^\/]*)\!([^\/]*)/$1<sup>$2<\/sup>/g;
+$word=~ s/([^\!][^\!]*)\!([^\!][^\!]*)/$1<sup>$2<\/sup>/g;
 return $word;
 }
 
